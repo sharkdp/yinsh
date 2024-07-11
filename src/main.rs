a7db6b8b7cb4d6e5fed5cc99343d4b8344f5e48a
David Peter <mail@david-peter.de>
2024-07-11 20:47:52 +0200

    Basic ring/marker placement

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,46 @@ use bevy::{
     prelude::*,
     render::view::RenderLayers,
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
-    window::WindowMode,
+    window::{PresentMode, PrimaryWindow, WindowMode},
 };
-use yinsh::Coord;
+use yinsh::{Coord, Player};
 
 #[derive(Component)]
-struct BoardElement(Coord);
+struct BoardElement(Coord, Player);
+
+#[derive(Component)]
+struct Ring;
+
+#[derive(Component)]
+struct Marker;
+
+#[derive(Component)]
+struct MainCamera;
+
+#[derive(Component)]
+struct CursorElement;
+
+#[derive(Resource)]
+pub enum GameState {
+    PlaceRing,
+    PlaceMarker,
+}
+
+#[derive(Resource)]
+pub struct MouseCursorCoord(Option<Coord>);
+
+const PLAYER_HUMAN: Player = Player::A;
+const PLAYER_COMPUTER: Player = Player::B;
+
+#[derive(Resource)]
+struct PlayerColors {
+    human: Handle<ColorMaterial>,
+    human_transparent: Handle<ColorMaterial>,
+    computer: Handle<ColorMaterial>,
+}
+
+const BACKGROUND_RENDER_LAYER: RenderLayers = RenderLayers::layer(1);
+const FOREGROUND_RENDER_LAYER: RenderLayers = RenderLayers::layer(2);
 
 fn main() {
     App::new()
@@ -22,6 +56,7 @@ fn main() {
                     name: Some("yinsh".into()),
                     resolution: (960., 960.).into(),
                     mode: WindowMode::Windowed,
+                    present_mode: PresentMode::Immediate,
                     ..default()
                 }),
                 ..default()
@@ -32,10 +67,43 @@ fn main() {
         .add_systems(Startup, setup)
         .add_systems(
             Update,
-            (draw_grid, board_element_positioning, keyboard_control),
+            (
+                determine_game_state,
+                draw_grid,
+                keyboard_control,
+                mouse_cursor_system,
+                mouse_interaction_system,
+                move_and_colorize_board_elements,
+            )
+                .chain(),
         )
         .insert_resource(ClearColor(Color::hsl(0.0, 0.0, 0.1)))
+        .insert_resource(Msaa::default())
+        .insert_resource(GameState::PlaceRing)
+        .insert_resource(MouseCursorCoord(None))
         .run();
+}
+
+fn ring_mesh(
+    meshes: &mut Assets<Mesh>,
+    color_material: Handle<ColorMaterial>,
+) -> MaterialMesh2dBundle<ColorMaterial> {
+    MaterialMesh2dBundle {
+        mesh: Mesh2dHandle(meshes.add(Annulus::new(SPACING / 4., SPACING / 3.))),
+        material: color_material,
+        ..default()
+    }
+}
+
+fn marker_mesh(
+    meshes: &mut Assets<Mesh>,
+    color_material: Handle<ColorMaterial>,
+) -> MaterialMesh2dBundle<ColorMaterial> {
+    MaterialMesh2dBundle {
+        mesh: Mesh2dHandle(meshes.add(Circle::new(SPACING / 5.))),
+        material: color_material,
+        ..default()
+    }
 }
 
 fn setup(
@@ -55,7 +123,7 @@ fn setup(
             ..default()
         },
         BloomSettings::default(),
-        RenderLayers::layer(1),
+        BACKGROUND_RENDER_LAYER,
     ));
 
     // Render layer 2 is for the board elements
@@ -69,36 +137,34 @@ fn setup(
             ..default()
         },
         BloomSettings::default(),
-        RenderLayers::layer(2),
+        FOREGROUND_RENDER_LAYER,
+        MainCamera,
     ));
+
+    let human_transparent = materials.add(Color::srgba(2., 2., 2., 0.5));
+    commands.insert_resource(PlayerColors {
+        human: materials.add(Color::srgba(2., 2., 2., 1.0)),
+        human_transparent: human_transparent.clone(),
+        computer: materials.add(Color::srgba(0.0, 0.0, 0.0, 1.0)),
+    });
 
     let (config, _) = config_store.config_mut::<DefaultGizmoConfigGroup>();
-    config.render_layers = RenderLayers::layer(1);
-
-    let color = Color::hsl(0., 0., 2.0);
-
-    let coord = Coord { x: 2, y: -1 };
+    config.render_layers = BACKGROUND_RENDER_LAYER;
 
     commands.spawn((
-        MaterialMesh2dBundle {
-            mesh: Mesh2dHandle(meshes.add(Circle {
-                radius: SPACING / 5.,
-            })),
-            material: materials.add(color),
-            ..default()
-        },
-        BoardElement(coord),
-        RenderLayers::layer(2),
+        ring_mesh(&mut meshes, human_transparent.clone()),
+        BoardElement(Coord { x: 0, y: 0 }, PLAYER_HUMAN),
+        Ring,
+        CursorElement,
+        FOREGROUND_RENDER_LAYER,
     ));
 
     commands.spawn((
-        MaterialMesh2dBundle {
-            mesh: Mesh2dHandle(meshes.add(Annulus::new(SPACING / 4., SPACING / 3.))),
-            material: materials.add(color),
-            ..default()
-        },
-        BoardElement(coord),
-        RenderLayers::layer(2),
+        marker_mesh(&mut meshes, human_transparent),
+        BoardElement(Coord { x: 0, y: 0 }, PLAYER_HUMAN),
+        Marker,
+        CursorElement,
+        FOREGROUND_RENDER_LAYER,
     ));
 }
 
@@ -110,6 +176,26 @@ fn screen_point(coord: Coord) -> Vec3 {
         SPACING * (-coord.y as f32 + 0.5 * coord.x as f32),
         0.,
     )
+}
+
+fn determine_game_state(
+    mut game_state: ResMut<GameState>,
+    q_rings: Query<&BoardElement, (With<Ring>, Without<CursorElement>)>,
+) {
+    let rings_human: Vec<_> = q_rings
+        .iter()
+        .filter(|e| e.1 == PLAYER_HUMAN)
+        .map(|e| e.0)
+        .collect();
+    let rings_computer: Vec<_> = q_rings
+        .iter()
+        .filter(|e| e.1 == PLAYER_COMPUTER)
+        .map(|e| e.0)
+        .collect();
+
+    if rings_human.len() >= 2 {
+        *game_state = GameState::PlaceMarker;
+    }
 }
 
 fn draw_grid(mut gizmos: Gizmos) {
@@ -161,9 +247,113 @@ fn draw_grid(mut gizmos: Gizmos) {
     }
 }
 
-fn board_element_positioning(mut query: Query<(&BoardElement, &mut Transform)>) {
-    for (BoardElement(coord), mut transform) in query.iter_mut() {
+fn move_and_colorize_board_elements(
+    mut query: Query<(&BoardElement, &mut Transform, &mut Handle<ColorMaterial>)>,
+    player_colors: Res<PlayerColors>,
+) {
+    for (BoardElement(coord, player), mut transform, mut color_material) in query.iter_mut() {
         transform.translation = screen_point(*coord);
+
+        *color_material = if player == &PLAYER_HUMAN {
+            player_colors.human.clone()
+        } else {
+            player_colors.computer.clone()
+        };
+    }
+}
+
+fn mouse_cursor_system(
+    q_window: Query<&Window, With<PrimaryWindow>>,
+    q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+    mut cursor_ring: Query<
+        (&mut BoardElement, &mut Visibility),
+        (With<Ring>, Without<Marker>, With<CursorElement>),
+    >,
+    mut cursor_marker: Query<
+        (&mut BoardElement, &mut Visibility),
+        (With<Marker>, Without<Ring>, With<CursorElement>),
+    >,
+    q_rings: Query<&BoardElement, (With<Ring>, Without<CursorElement>)>,
+    game_state: Res<GameState>,
+    mut mouse_cursor_coord: ResMut<MouseCursorCoord>,
+) {
+    if let Some(cursor_position) = q_window.single().cursor_position() {
+        let (camera, camera_transform) = q_camera.single();
+
+        if let Some(cursor_position) = camera
+            .viewport_to_world(camera_transform, cursor_position)
+            .map(|ray| ray.origin.truncate())
+        {
+            let rings_human = q_rings
+                .iter()
+                .filter(|e| e.1 == PLAYER_HUMAN)
+                .map(|e| e.0)
+                .collect::<Vec<_>>();
+
+            let cursor_coord = yinsh::all_coords()
+                .into_iter()
+                .min_by_key(|c| {
+                    let screen_pos = screen_point(*c);
+                    let cursor_pos = Vec3::new(cursor_position.x, cursor_position.y, 0.0);
+                    let diff = screen_pos - cursor_pos;
+                    diff.length_squared() as i32
+                })
+                .unwrap();
+
+            let (mut cursor_ring_coord, mut cursor_ring_visibility) = cursor_ring.single_mut();
+            *cursor_ring_visibility = Visibility::Hidden;
+
+            let (mut cursor_marker_coord, mut cursor_marker_visibility) =
+                cursor_marker.single_mut();
+            *cursor_marker_visibility = Visibility::Hidden;
+
+            match *game_state {
+                GameState::PlaceRing => {
+                    *cursor_ring_visibility = Visibility::Visible;
+                    cursor_ring_coord.0 = cursor_coord;
+                }
+                GameState::PlaceMarker => {
+                    if rings_human.contains(&cursor_coord) {
+                        *cursor_marker_visibility = Visibility::Visible;
+                        cursor_marker_coord.0 = cursor_coord;
+                    }
+                }
+            }
+
+            mouse_cursor_coord.0 = Some(cursor_coord);
+        }
+    }
+}
+
+fn mouse_interaction_system(
+    buttons: Res<ButtonInput<MouseButton>>,
+    game_state: Res<GameState>,
+    mouse_cursor_coord: Res<MouseCursorCoord>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    player_colors: Res<PlayerColors>,
+) {
+    if let Some(mouse_cursor_coord) = mouse_cursor_coord.0 {
+        if buttons.just_pressed(MouseButton::Left) {
+            match *game_state {
+                GameState::PlaceRing => {
+                    commands.spawn((
+                        ring_mesh(&mut meshes, player_colors.human.clone()),
+                        BoardElement(mouse_cursor_coord, PLAYER_HUMAN),
+                        Ring,
+                        FOREGROUND_RENDER_LAYER,
+                    ));
+                }
+                GameState::PlaceMarker => {
+                    commands.spawn((
+                        marker_mesh(&mut meshes, player_colors.human.clone()),
+                        BoardElement(mouse_cursor_coord, PLAYER_HUMAN),
+                        Marker,
+                        FOREGROUND_RENDER_LAYER,
+                    ));
+                }
+            }
+        }
     }
 }
 
