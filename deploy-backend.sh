5356006785539438e1e1b5073531c5a7d63a3eb1
David Peter <mail@david-peter.de>
2025-12-20 22:17:59 +0100

    Remove outdated files

--- deploy-backend.sh
+++
@@ -1,2 +0,0 @@
-cabal-3.6.2.0 build
-rsync --archive --stats --progress --human-readable dist-newstyle/build/x86_64-linux/ghc-9.2.5/yinsh-0.2.0.0/x/yinsh-backend/build/yinsh-backend/yinsh-backend shark.fish:yinsh/yinsh-backend
--- deploy-frontend.sh
+++
@@ -1,1 +0,0 @@
-rsync --archive --stats --progress --human-readable index.html frontend.js main.css shark.fish:david-peter.de/yinsh/
